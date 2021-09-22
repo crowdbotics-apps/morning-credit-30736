657348cbc2a540d8179f79037038e5fca06e8a27
Crowdbotics <team@crowdbotics.com>
2021-09-22 02:01:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "morning_credit_30736Identifier",
+  url: "https://morning_credit_30736Identifier.botics.co",
+  api: "https://morning_credit_30736Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
